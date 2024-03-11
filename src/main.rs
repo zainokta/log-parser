928216050bec49dcba22dc985d744bf755d4a166
zainokta <zainokta@gmail.com>
2024-03-11 14:26:29 +0700

    feat: log parser

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::{
+    fs::{self, File},
+    io::Read,
+    time::Instant,
+};
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Metrics {
+    // timestamp: String,
+    // level_name: String,
+    value: u32,
+}
+
+fn main() -> std::io::Result<()> {
+    let start = Instant::now();
+    let dir = fs::read_dir("D:\\Project\\log-parser\\metrics").unwrap();    
+
+    let mut counter: u32 = 0;
+
+    for file in dir {
+        let mut content = String::new();
+        File::open(file.unwrap().path())?.read_to_string(&mut content)?;
+
+        let result: Vec<Metrics> = serde_json::from_str(&content)?;
+
+        for item in result {
+            counter += item.value;
+        }
+    }
+    println!("{}", counter);
+    let duration = start.elapsed();
+
+    println!("time elasped: {:?}", duration);
+
+    Ok(())
+}
